afea76cb046e7c44417fbfdfecf48f85021c0f91
Yefry Gabriel García Caxaj <caxajgabriel2@gmail.com>
2025-08-17 02:39:18 -0600

    Final -> instancia de la clase Ordenador

--- Ordenador.py
+++ Ordenador.py
@@ -32,42 +32,5 @@ class Ordenador:
 
         return self.por_nombre(menores) + iguales + self.por_nombre(mayores)
 
+# Instanciar la clase Ordenador
 ordenador = Ordenador()
-
-class Producto:
-    def __init__(self, nombre, precio, stock):
-        self.nombre = nombre
-        self.precio = precio
-        self.stock = stock
-
-    def mostrarProducto(self):
-        return f"{self.nombre} {self.precio} {self.stock}"
-
-
-lista = {
-    "001": Producto("Manzana", 1.5, 80),
-    "002": Producto("Banana", 1.0, 150),
-    "003": Producto("Naranja", 2.0, 100),
-    "004": Producto("Pera", 1.2, 200),
-    "005": Producto("Uva", 3.0, 60)
-}
-
-lista_productos = list(lista.values())
-print("Lista original:")
-for producto in lista_productos:
-    print(producto.mostrarProducto())
-
-print("\nOrdenando por nombre:")
-lista_ordenada_nombre = ordenador.por_nombre(lista_productos)
-for producto in lista_ordenada_nombre:
-    print(producto.mostrarProducto())
-
-print("\nOrdenando por precio:")
-lista_ordenada_precio = ordenador.por_precio(lista_productos)
-for producto in lista_ordenada_precio:
-    print(producto.mostrarProducto())
-
-print("\nOrdenando por stock:")
-lista_ordenada_stock = ordenador.por_stock(lista_productos)
-for producto in lista_ordenada_stock:
-    print(producto.mostrarProducto())
