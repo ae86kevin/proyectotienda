d6e25b5482364ae9bb64026c65ea2d70c773e9bc
Yefry Gabriel García Caxaj <caxajgabriel2@gmail.com>
2025-08-17 02:37:49 -0600

    pruebas para ordenador /// NO FUNCIONA SOLO CON 1 ITEM

--- Ordenador.py
+++ Ordenador.py
@@ -1,7 +1,6 @@
 class Ordenador:
     def por_nombre(self, lista):
         if len(lista) <= 1:
-            print("...Lista vacia o con un solo elemento")
             return lista
 
         pivote = lista[0].nombre
@@ -13,7 +12,6 @@ class Ordenador:
 
     def por_precio(self, lista):
         if len(lista) <= 1:
-            print("...Lista vacia o con un solo elemento")
             return lista
 
         pivote = lista[0].precio
@@ -25,7 +23,6 @@ class Ordenador:
 
     def por_stock(self, lista):
         if len(lista) <= 1:
-            print("...Lista vacia o con un solo elemento")
             return lista
 
         pivote = lista[0].stock
@@ -34,3 +31,43 @@ class Ordenador:
         mayores = [x for x in lista[1:] if x.stock > pivote]
 
         return self.por_nombre(menores) + iguales + self.por_nombre(mayores)
+
+ordenador = Ordenador()
+
+class Producto:
+    def __init__(self, nombre, precio, stock):
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+
+    def mostrarProducto(self):
+        return f"{self.nombre} {self.precio} {self.stock}"
+
+
+lista = {
+    "001": Producto("Manzana", 1.5, 80),
+    "002": Producto("Banana", 1.0, 150),
+    "003": Producto("Naranja", 2.0, 100),
+    "004": Producto("Pera", 1.2, 200),
+    "005": Producto("Uva", 3.0, 60)
+}
+
+lista_productos = list(lista.values())
+print("Lista original:")
+for producto in lista_productos:
+    print(producto.mostrarProducto())
+
+print("\nOrdenando por nombre:")
+lista_ordenada_nombre = ordenador.por_nombre(lista_productos)
+for producto in lista_ordenada_nombre:
+    print(producto.mostrarProducto())
+
+print("\nOrdenando por precio:")
+lista_ordenada_precio = ordenador.por_precio(lista_productos)
+for producto in lista_ordenada_precio:
+    print(producto.mostrarProducto())
+
+print("\nOrdenando por stock:")
+lista_ordenada_stock = ordenador.por_stock(lista_productos)
+for producto in lista_ordenada_stock:
+    print(producto.mostrarProducto())
