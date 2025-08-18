ca53ca3d7e9a96db5eae4d872e6e7e45f91d60b3
Yefry Gabriel García Caxaj <caxajgabriel2@gmail.com>
2025-08-17 18:31:26 -0600

    refactorizacion -> clase Producto | retornar cadena de texto con los atributos de la clase

--- claseProducto.py
+++ claseProducto.py
@@ -7,5 +7,5 @@ class Producto:
         self.stock = stock
 
     def mostrarProducto(self):
-        return self.codigo,self.nombre,self.categoria,self.precio,self.stock
+        return f"Codigo: {self.codigo} Nombre: {self.nombre} Categoria: {self.categoria} Precio: {self.precio} Stock: {self.stock}"
 
